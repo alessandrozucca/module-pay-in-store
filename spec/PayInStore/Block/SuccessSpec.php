a0b853b9a00b5c818ff8cda6dd24525c6c3e9e86
Alessandro <alessandro.zucca@gmail.com>
2016-08-17 15:45:03 +0100

    Showing pickup code on success  - Adding spec for Success Block  - Code only shown when it's available

--- spec/PayInStore/Block/SuccessSpec.php
+++ spec/PayInStore/Block/SuccessSpec.php
@@ -7,8 +7,31 @@ use Prophecy\Argument;
 
 class SuccessSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+
+    function let(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Checkout\Model\Session $checkoutSession
+    )
     {
-        $this->shouldHaveType('MageSchool\PayInStore\Block\Success');
+        $this->beConstructedWith(
+            $context,
+            $checkoutSession,
+            []);
+    }
+
+    function it_returns_the_assigned_pickup_code_from_lastest_payment(
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Sales\Model\Order $lastOrder,
+        \Magento\Sales\Model\Order\Payment $payment
+    )
+    {
+        $checkoutSession->getLastRealOrder()->willReturn($lastOrder);
+        $lastOrder->getPayment()->willReturn($payment);
+        $payment->getAdditionalInformation()->willReturn(['store_pickup_code' => 'ABCDEFG']);
+
+        $this->getStorePickupCodeFromLastPayment()->shouldBe("ABCDEFG");
+
+        $checkoutSession->getLastRealOrder()->shouldHaveBeenCalled();
+        $lastOrder->getPayment()->shouldHaveBeenCalled();
     }
 }
--- src/PayInStore/Block/Success.php
+++ src/PayInStore/Block/Success.php
@@ -2,11 +2,51 @@
 namespace MageSchool\PayInStore\Block;
 
 use Magento\Framework\View\Element\Template;
+use \Magento\Checkout\Model\Session;
 
 class Success extends Template
 {
-    public function getStorePickupBlock()
-    {
 
+    /**
+     * @var Session
+     */
+    private $checkoutSession;
+
+    public function __construct(
+        Template\Context $context,
+        Session $checkoutSession,
+        array $data)
+    {
+        $this->checkoutSession = $checkoutSession;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getStorePickupCodeFromLastPayment()
+    {
+        $lastPayment = $this->getLastPayment();
+        if ($lastPayment) {
+            return $this->getStorePickupCode($lastPayment);
+        }
+        return '';
+    }
+
+
+    private function getStorePickupCode(\Magento\Sales\Api\Data\OrderPaymentInterface $payment)
+    {
+        $additionalInformation =  $payment->getAdditionalInformation();
+        return $additionalInformation['store_pickup_code'];
+    }
+
+    /**
+     * @return \Magento\Sales\Api\Data\OrderPaymentInterface|null
+     */
+    private function getLastPayment()
+    {
+        /** @var \Magento\Sales\Api\Data\OrderInterface $order */
+        $order = $this->checkoutSession->getLastRealOrder();
+        return $order->getPayment();
     }
 }
